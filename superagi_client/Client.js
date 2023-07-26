a6fb9f0d0e1af9c91ac7ffc43ca53af9e00ce0b6
a35z <112294447+a35z@users.noreply.github.com>
2023-07-26 20:18:46 +0000

    Add installation instruction

--- superagi_client/Client.js
+++ superagi_client/Client.js
@@ -1,1 +1,9 @@
-const { validateApiKey } = require('./
+const { validateApiKey } = require('./lib/Auth');
+const Superagi = require('./lib/Superagi');
+const { AgentConfig, AgentUpdateConfig, AgentRun, AgentRunFilter } = require('./Types');
+
+class Client {
+    /**
+     * Initialize the Client.
+     *
+     * @param {Object} options - The options for the client.
