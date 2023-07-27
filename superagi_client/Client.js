74fa2b031205ec316f702d506f4f6bafb3bc5315
a35z <112294447+a35z@users.noreply.github.com>
2023-07-27 05:52:11 +0000

    Changed podspec

--- superagi_client/Client.js
+++ superagi_client/Client.js
@@ -7,3 +7,7 @@ class Client {
      * Initialize the Client.
      *
      * @param {Object} options - The options for the client.
+     * @param {string} options.apiKey - The API key for authentication.
+     * @param {string} [options.url="https://app.superagi.com"] - The URL of the Superagi service.
+     * @param {Superagi} [options.superagi=null] - An instance of the Superagi class.
+     * @param {Object} options.kwargs
