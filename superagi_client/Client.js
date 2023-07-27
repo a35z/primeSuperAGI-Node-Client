67954a4e0e682fe3260420fb5f6006a0c1e798c3
a35z <112294447+a35z@users.noreply.github.com>
2023-07-27 05:57:42 +0000

    Update example in README

--- superagi_client/Client.js
+++ superagi_client/Client.js
@@ -10,4 +10,12 @@ class Client {
      * @param {string} options.apiKey - The API key for authentication.
      * @param {string} [options.url="https://app.superagi.com"] - The URL of the Superagi service.
      * @param {Superagi} [options.superagi=null] - An instance of the Superagi class.
-     * @param {Object} options.kwargs
+     * @param {Object} options.kwargs - Additional keyword arguments.
+     */
+    constructor({
+                    apiKey,
+                    url = "https://app.superagi.com",
+                    superagi = null,
+                    ...kwargs
+                } = {}) {
+        if (typeof apiKey !== 'string') throw new TypeError('apiKey is 
